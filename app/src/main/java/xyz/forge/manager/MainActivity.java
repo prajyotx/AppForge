f1a282a5c48a9819a2c680c7abe7a8e922501162
prajyotx <thats.prajyotxd@gmail.com>
2024-05-13 01:47:11 +0530

    Added SplashScreen Updated Settings.gradle Updated gradleVersion from root gradle updated buildtoolVersion to 34.0.4 Added Ndk

--- app/src/main/java/xyz/forge/manager/MainActivity.java
+++ app/src/main/java/xyz/forge/manager/MainActivity.java
@@ -1,27 +1,46 @@
-
 package xyz.forge.manager;
 
+import android.graphics.Typeface;
+import android.os.Build;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import com.airbnb.lottie.LottieAnimationView;
+import android.view.Window;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import xyz.forge.manager.R;
 import xyz.forge.manager.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
+    private LinearLayout linear1;
+    private TextView textview1;
+    private LottieAnimationView lottie1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        // Inflate and get instance of binding
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        linear1 = findViewById(R.id.linear1);
+        textview1 = findViewById(R.id.textview1);
+        lottie1 = findViewById(R.id.lottie1);
 
-        // set content view to binding's root
-        setContentView(binding.getRoot());
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+            Window w = MainActivity.this.getWindow();
+            w.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            w.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            w.setStatusBarColor(0xFF111111);
+        }
+        
+        textview1.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/chillax_semi_bold.ttf"));
+        
     }
-    
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        this.binding = null;
     }
 }
